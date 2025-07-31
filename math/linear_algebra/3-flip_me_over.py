c1b7b77434b9b9f3a5bd35f3f0f612a7dc6e9b68
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-07-31 13:38:05 +0000

    feat: task 3 transpose 2D matrix

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Transpose a 2D matrix
+"""
+
+
+def matrix_transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    transposed = []
+    for j in range(cols):
+        new_row = []
+        for i in range(rows):
+            new_row.append(matrix[i][j])
+        transposed.append(new_row)
+
+    return transposed
