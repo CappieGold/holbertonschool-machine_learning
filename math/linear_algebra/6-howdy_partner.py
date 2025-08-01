2755e873bab77c83b8cccc1a3ea1123e904c0f31
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-08-01 12:56:05 +0000

    feat: task 6 concatenate 2 array

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Howdy Partner
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Write a function def cat_arrays(arr1, arr2):
+      that concatenates two arrays
+    """
+
+    new_list = []
+    new_list.extend(arr1)
+    new_list.extend(arr2)
+    return new_list
